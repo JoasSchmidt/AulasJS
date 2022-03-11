5187b96914ddad6aba18b2376105d5c93deaea99
JoasSchmidt <101359176+JoasSchmidt@users.noreply.github.com>
2022-03-11 15:39:28 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,42 @@
-var pessoa
+
 function setup() {
   createCanvas(400,400);
-  pessoa = createSprite(30,200,10,14);
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background("purple");
-if(keyDown("d")){
-  pessoa.x = pessoa.x +2;
+
+  // escreva o código para alterar a cor de fundo 
+  // para vermelho quando a seta para direita (RIGHT_ARROW) for pressionada
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("red");
+    
+  }
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
 }
-if(keyDown("a")){
-  pessoa.x = pessoa.x -2;
-}
-
-drawSprites()
-}
-
-
-
 
